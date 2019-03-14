d52c44d4c94294c6fd99a2517f2c766a16aa268c
T1508BRN <t1508brn@prestadorcbmp.com.br>
2019-03-14 17:06:33 -0300

    testando commit local c

--- src/main/java/br/com/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/br/com/restfulwebservice/RestfulWebServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
+	// teste
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServiceApplication.class, args);
 	}
