27d823e226f3bbaab2027f4868d1f5deb9dd78d1
T1508BRN <t1508brn@prestadorcbmp.com.br>
2019-03-25 18:47:27 -0300

    addd class de configuração de properties

--- limits-service/src/main/java/br/com/limitsservice/config/LimitsConfigurationController.java
+++ limits-service/src/main/java/br/com/limitsservice/config/LimitsConfigurationController.java
@@ -1,17 +1,22 @@
 package br.com.limitsservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.limitsservice.bean.Configuration;
 import br.com.limitsservice.bean.LimitConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 		
 	}
 
