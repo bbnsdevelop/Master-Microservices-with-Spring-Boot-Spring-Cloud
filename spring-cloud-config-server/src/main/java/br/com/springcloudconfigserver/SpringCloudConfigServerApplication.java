bfcbd5958c2715be34b28318765c8c77dc843663
T1508BRN <t1508brn@prestadorcbmp.com.br>
2019-04-02 17:27:12 -0300

    add ambients de dev, qa e default

--- spring-cloud-config-server/src/main/java/br/com/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/br/com/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,9 +2,9 @@ package br.com.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-//@EnableDiscoveryClient
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
