bfb4012033377b74b9e6c1ec34c8ec530dfbd9e9
T1508BRN <t1508brn@prestadorcbmp.com.br>
2019-04-09 15:57:50 -0300

    add log for track back on call

--- currency-conversion-service/src/main/java/br/com/currencyconversionservice/service/impl/CurrencyServiceImpl.java
+++ currency-conversion-service/src/main/java/br/com/currencyconversionservice/service/impl/CurrencyServiceImpl.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +16,8 @@ import br.com.currencyconversionservice.service.CurrencyService;
 
 @Service
 public class CurrencyServiceImpl implements CurrencyService{
+	
+	private static final Logger log = LoggerFactory.getLogger(CurrencyServiceImpl.class);
 
 	@Override
 	public CurrencyConversionBean convert(String from, String to, BigDecimal qtd) {
@@ -20,7 +25,7 @@ public class CurrencyServiceImpl implements CurrencyService{
 		Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
-		
+		log.info("calling currency-exchange-service method retriveExchangeValue");
 		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000//currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
 		CurrencyConversionBean respose = responseEntity.getBody();
 		
--- currency-exchange-service/src/main/java/br/com/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/br/com/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -1,9 +1,6 @@
 package br.com.currencyexchangeservice.controllers;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
