21aef0d9cfc6196b201630a8b18c367181fd58e8
Bruno Batista <bbnsdevelop@gmail.com>
2019-05-02 11:38:57 -0300

    add spring boot admin to monitoring micro services

---
+++ spring-boot-admin/src/main/java/br/com/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,18 @@
+package br.com.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
